3cfc72c339c4a4f1c868558d8b31dbc75a536ac2
jenweani <jenweani@gmail.com>
2023-03-16 16:40:29 -0700

    added printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
+/*
+ * File: 4-puts.c
+ * Auth: Enweani John <jenweani@gmail.com>
+*/
+
 #include <stdio.h>
 
+/*
+ * main - Prints "Programmini is like building a multilingual puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+*/
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,14 @@
+/*
+ * File: 5-printf.c
+ * Auth: Enweani John <jenweani@gmail.com
+*/
+
 #include <stdio.h>
 
+/*
+ * main - prints "with proper grammar, but the outcome is a piece of art,"followed by a new line
+ * Return: Always returns 0
+*/
 int main(void){
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
