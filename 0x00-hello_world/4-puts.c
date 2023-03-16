e7d31bffc8744f18ffac0e0d2f4cff02a7842afd
jenweani <jenweani@gmail.com>
2023-03-16 16:45:04 -0700

    update print file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,11 +6,13 @@
 #include <stdio.h>
 
 /*
- * main - Prints "Programmini is like building a multilingual puzzle, followed by a new line.
+ * main - Prints "Programmini is like building a multilingual puzzle,
+ *        followed by a new line.
  *
  * Return: Always 0.
 */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,12 @@
 #include <stdio.h>
 
 /*
- * main - prints "with proper grammar, but the outcome is a piece of art,"followed by a new line
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * 	  followed by a new line
  * Return: Always returns 0
 */
-int main(void){
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
