6888b81fd0cad51d753bbbe31beb507bfce40bc7
jenweani <jenweani@gmail.com>
2023-03-16 16:51:50 -0700

    update print file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,8 @@
 
 #include <stdio.h>
 
-/*
- * main: prints "with proper grammar, but the outcome is a piece of art,"
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
  *       followed by a new line
  * Return: Always returns 0
 */
