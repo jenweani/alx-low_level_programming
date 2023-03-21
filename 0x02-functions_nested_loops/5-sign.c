b5d58fe9b60e30e82c666bdacf5f14727b58fe0d
jenweani <jenweani@gmail.com>
2023-03-21 12:20:56 -0700

    sign task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+/*
+ * File: 5-sign.c
+ * Author: John Enweani
+*/
+
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @c: the input int 
+ *
+ * Return: returns + if the input is greater than zero, 0 if input is zero and - if input is less than zero
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
