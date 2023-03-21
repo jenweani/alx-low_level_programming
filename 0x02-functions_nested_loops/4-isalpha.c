2133256b24a5c5bd2f048b7ab271d94a21680cca
jenweani <jenweani@gmail.com>
2023-03-21 11:04:58 -0700

    isalpha task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-isalpha.c
+ * Author: John Enweani
+*/
+
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - checks if the input is an alphabet
+ * @c - input character
+ *
+ * Returns: 1 if input is alphabet, 0 otherwise
+*/
+
+int _isalpha (char c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
