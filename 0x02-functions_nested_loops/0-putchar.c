5569a7138d276bd6fab02204036f2919d35eb64a
jenweani <jenweani@gmail.com>
2023-03-21 06:58:14 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-putchar.c
+ * Author: John Enweani <jenweani@gmail.com>
+*/
+
+#include <stdio.h>
+
+/**
+ * main - Prints out _putchar
+ * Return: Always 0
+*/
+
+int main(void) 
+{
+	_putchar("_putchar");
+	return 0;
+}
