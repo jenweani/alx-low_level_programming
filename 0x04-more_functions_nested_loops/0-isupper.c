7c91d9f5d6873bce6f87580b05a67bb8e822dbed
jenweani <jenweani@gmail.com>
2023-03-23 02:59:28 -0700

    isupper task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-isupper.c
+ * Author: John Enweani
+*/
+
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - checks if the input is an uppercase alphabet
+ *
+ * Return: 1 if the input is uppercase, 0 otherwise
+*/
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
