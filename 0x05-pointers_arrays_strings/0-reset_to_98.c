a79389caaa7c3562611d96344699b69d8f28cb2c
jenweani <jenweani@gmail.com>
2023-03-27 06:40:35 -0700

    added swap task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0-reset_to_98.c
+ * Author: John Enweani
+*/
+
+#include "main.h"
+
+/**
+ * reset_to_98 - sets the value of the int that was passed as arg to 98
+ * @n: integer to be reseted
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
