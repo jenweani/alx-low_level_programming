c25a31d295dc462f5aa775f5dba112239b4e3269
jenweani <jenweani@gmail.com>
2023-03-27 08:14:14 -0700

    added rev_string task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,8 +16,8 @@ void rev_string (char *s)
 {
 	int i;
 	int len = strlen(s);
-	char s_cpy[len] = s;
+	char s_cpy[len] = *s;
 
 	for (i = len - 1; i >= 0; i--)
-		*s[len-1-i] = s_cpy[i];
+		s[len-1-i] = s_cpy[i];
 }
