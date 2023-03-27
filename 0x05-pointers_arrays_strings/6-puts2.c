a7c604d6ef7a80f5e81bcbfff44cff07ea3a76de
jenweani <jenweani@gmail.com>
2023-03-27 09:04:26 -0700

    added puts2 task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+/*
+ * File: 6-puts2.c
+ * Author: john Enweani
+*/
+
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: input string arg
+*/
+
+void puts@(char *str)
+{
+	int i = 0, len = 0;
+
+	while (str[i++])
+		len++;
+
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
