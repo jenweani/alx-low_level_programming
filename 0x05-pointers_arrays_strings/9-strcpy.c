66db905c7cb1757d37cb8d646df24e7f8e0e2029
jenweani <jenweani@gmail.com>
2023-03-27 09:29:08 -0700

    added strcpy task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -17,11 +17,11 @@ char *_strcpy(char *dest, const char *src)
 {
 	int index = 0;
 
-	while (src[index])
+	while (*src != '\0')
 	{
-		dest[index] = src[index];
+		dest[index] = *src;
 		index++;
+		src++;
 	}
-
 	return (dest);
 }
