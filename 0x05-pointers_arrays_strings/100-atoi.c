286e3b08b1d223192eb1e4083973c5a95f4227c0
jenweani <jenweani@gmail.com>
2023-03-27 09:48:13 -0700

    addded atoi task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+/*
+ * File: 100-atoi.c
+ * Author: John Enweani
+*/
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - converts a string to an int
+ * @s: string to be converted
+ *
+ * Return: returns the int equivalent of the string
+*/
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
