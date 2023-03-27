755d8393f5f4dd96bbe8a90ef04ec41dc7b4a426
jenweani <jenweani@gmail.com>
2023-03-27 06:32:10 -0700

    added swap task

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,10 +3,12 @@
  * Author: John Enweani
 */
 
+#include "main.h"
+
 /**
  * swap_int - swaps the value of two integers.
- * @*a: collects address of int 
- * @*b: collects address of int
+ * @a: collects address of int 
+ * @b: collects address of int
 */
 
 void swap_int(int *a, int *b)
