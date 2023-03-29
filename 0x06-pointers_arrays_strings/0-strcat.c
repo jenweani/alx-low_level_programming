01287755ea46150fd3b7cae393a62b568c443020
jenweani <jenweani@gmail.com>
2023-03-29 15:50:27 -0700

    added strcat task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,13 +16,7 @@
 
 char *_strcat (char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	strcat(dest, src);
 
 	return (dest);
 }
