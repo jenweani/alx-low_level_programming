ee5eef880f24f995cc419e0f58031f86c1145847
jenweani <jenweani@gmail.com>
2023-04-03 13:06:42 -0700

    fix strpbrk task

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,18 +15,19 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int index;
+	int i;
 
 	while (*s)
 	{
-		for (index = 0; accept[index]; index++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (*s == accept[index])
+			if (*s == accept[i])
 				return (s);
 		}
 
 		s++;
 	}
+
 
 	return ('\0');
 }
