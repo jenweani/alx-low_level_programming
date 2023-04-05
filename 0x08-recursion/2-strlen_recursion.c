b6260e28581be37d7b824f08f138f9d68bc05b11
jenweani <jenweani@gmail.com>
2023-04-05 07:51:18 -0700

    added strlen recursion task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Author: John Enweani
+*/
+
+#include "main.h"
+
+/**
+ * _strlen_recursion(char *s) - function that returns
+ * the length of a string.
+ * @s: string that the length will be returned
+ *
+ * Return: the length of the string
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+	return (0);
+}
