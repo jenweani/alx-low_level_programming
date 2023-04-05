ed997ef9f4e91d13847dbcd89b12396c42da1b9b
jenweani <jenweani@gmail.com>
2023-04-05 04:14:17 -0700

    added puts recursion task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+/*
+ * File: 0-puts_recursion.c
+ * Author: John Enweani
+*/
+
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string to be printe
+*/
+void _puts_recursion(char *s)
+{
+	if(*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
