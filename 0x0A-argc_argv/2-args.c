60ae91711ebaeb7004a599cd92da28f4318e5aab
jenweani <jenweani@gmail.com>
2023-04-07 05:16:04 -0700

    added 2-args task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-args.c
+ * Author: John Enweani
+*/
+
+#include <stdio.h>
+
+/**
+ * main - prints all the arguments recieved
+ * @argc: argument count
+ * @argv: argument variables
+ *
+ * Return: always 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
