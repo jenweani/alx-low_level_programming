22dbc749ad900b5c148f6f8fe37faba99c7c61bb
jenweani <jenweani@gmail.com>
2023-04-07 04:23:33 -0700

    added whatsmyname task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,17 @@
 
 #include <stdio.h>
 
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: argument count
+ * @argv: argument variables
+ *
+ * Return: Always 0
+*/
+
 int main(int argc, char *argv[])
 {
-	printf("%s", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
