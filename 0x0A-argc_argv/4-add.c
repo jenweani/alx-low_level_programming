9a536f675950bb24b3470bfd9a8317bf06ed9d77
jenweani <jenweani@gmail.com>
2023-04-08 04:25:35 -0700

    added 4add task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+/*
+ * File: 4-add.c
+ * Author: John Enweani
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 1 if one of the number contains symbols that are not digits
+ *  0 otherwise
+*/
+int main(int argc, char *argv[])
+{
+	int i, j, result = 0;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+
+	return (0);
+}
