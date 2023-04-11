f3b77ac53221fd5b51db8e0fccc3609f6864e467
jenweani <jenweani@gmail.com>
2023-04-11 08:05:08 -0700

    fix handle retun malloc error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,6 +22,10 @@ char *create_array(unsigned int size, char c)
 	if ((int) size <= 0)
 		return (NULL);
 	arr = malloc(sizeof(char) * size);
+
+	if (arr == NULL)
+		return (NULL);
+
 	while (i < size)
 	{
 		arr[i] = c;
