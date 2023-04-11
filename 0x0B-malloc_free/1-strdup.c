0a4513f0e61c6177f133f30a82e656e42260c598
jenweani <jenweani@gmail.com>
2023-04-11 08:21:09 -0700

    added strdup task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+/*
+ * File: 1-strdup.c
+ * Auth: John Enweani
+ */
+
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *           which contains a copy of the string given as a parameter
+ * @str: new string which is a duplicate of the string
+ *
+ * Return: returns a pointer to a newly allocated space in memory
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i, length = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		length++;
+
+	dup = malloc(sizeof(char) * (length + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		dup[i] = str[i];
+
+	dup[length] = '\0';
+
+	return (dup);
+}
