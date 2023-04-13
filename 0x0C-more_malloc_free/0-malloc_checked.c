9382104c989970c806e314c7ed42ec451f35e880
jenweani <jenweani@gmail.com>
2023-04-13 03:51:18 -0700

    initial commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+/*
+ * File: 0-malloc_checked.c
+ * Auth: John Enweani
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * 0-malloc_checked.c -  function that allocates memory using malloc
+ * @b: size of memory to be allocated
+ *
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *memory = malloc(b);
+
+	if (memory == NUL)
+		exit(98);
+
+	return (memory);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This repository contains solutions to alx more malloc free tasks
