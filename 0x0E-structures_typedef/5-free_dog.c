f6d7bf070c2bd9eb82a486be8e8037cca3f9d9b4
jenweani <jenweani@gmail.com>
2023-04-17 19:44:57 -0700

    added free dog task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+/*i
+ * File: 5-free_dog.c
+ * Auth: John Enweani
+ */
+
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer to dog stuct in memory.
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (!d)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
