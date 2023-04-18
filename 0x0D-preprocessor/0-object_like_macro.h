8f2aa0f9410f4854c7185712eda08250189cd14a
jenweani <jenweani@gmail.com>
2023-04-17 17:30:21 -0700

    added object like macro task

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,7 +2,7 @@
 #define OBJECT_LIKE_MACRO_H
 
 /*
- * File: 0-object_like_macro.h
+ * File - 0-object_like_macro.h
  * Description - header file that defines a macro named SIZE
  * as an abbreviation from the token 1024.
  */
