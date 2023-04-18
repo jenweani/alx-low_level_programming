1e8054a39b04fb46b7e4eab1391dc6ca829a291a
jenweani <jenweani@gmail.com>
2023-04-17 19:35:43 -0700

    added new dog task

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,59 @@
+/*
+ * File: 4-new_dog.c
+ * Auth: John Enweani
+ */
+
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strcpy - copy read only data to mutatable.
+ * @dest: pointer to copy char to.
+ * @src: read only data.
+ */
+void _strcpy(char *dest, char *src)
+{
+	int j;
+
+	for (j = 0; src[j]; j++)
+		dest[j] = src[j];
+	dest[j] = '\0';
+}
+
+/**
+ * new_dog - create new dog from the dna of the first dog.
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int a, b;
+
+	dog = malloc(sizeof(dog_t));
+	if (!dog)
+		return (NULL);
+
+	dog->name = malloc(a + 1);
+	if (!dog->name)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(b + 1);
+	if (!dog->owner)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+	return (dog);
+}
