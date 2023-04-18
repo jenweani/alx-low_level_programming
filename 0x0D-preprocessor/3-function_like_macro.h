36e784c33f57efb6e53cb1b62955874904d4783c
jenweani <jenweani@gmail.com>
2023-04-17 21:58:22 -0700

    fix function like macro task

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,13 @@
-#ifndef ABS_H
-#define ABS_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) (x < 0 ? x * (-1) : (x))
+/*
+ * File: 3-function_like_macro.h
+ * Auth: John Enweani
+ * Desc: Header file that defines a function-like macro ABS(x)
+ *       that computes the absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? (x) * (-1) : (x))
 
 #endif
