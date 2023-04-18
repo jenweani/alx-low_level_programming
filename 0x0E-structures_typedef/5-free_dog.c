d6461b9a7db8f97c52bc55ae9a4570a3a7400911
jenweani <jenweani@gmail.com>
2023-04-17 19:49:42 -0700

    added free dog task

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,13 +9,12 @@
 /**
  * free_dog - function that frees dogs
  * @d: pointer to dog stuct in memory.
- *
- * Return: nothing
  */
 void free_dog(dog_t *d)
 {
 	if (!d)
 		return;
+
 	free(d->name);
 	free(d->owner);
 	free(d);
