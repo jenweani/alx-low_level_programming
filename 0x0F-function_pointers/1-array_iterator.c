2dc7e6a521a8d6cbb2dee4aa79ba1df2dddb3d89
jenweani <jenweani@gmail.com>
2023-04-19 19:12:10 -0700

    added array iterator task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+/*
+ * File: 1-array_iterator.c
+ * Auth: John Enweani
+ */
+
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that executes a function given
+ *  as a parameter on each element of an array
+ * @array: array to be modified
+ * @size: size of the array
+ * @action: function pointer to func to be executed on each parameter*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!action)
+		return;
+
+	for (i = 0; i < (int) size; i++)
+		(*action)(array[i]);
+}
