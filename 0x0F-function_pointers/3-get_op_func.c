07fa2c17868431e2ced3e26274c805e027a54b3e
jenweani <jenweani@gmail.com>
2023-04-19 20:39:35 -0700

    added 3-calc tasks

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+/*
+ * File: 3-get_op_func.c
+ * Auth: John Enweani
+ */
+
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - Selects the correct function to perform
+ *  the operation asked by the user
+ * @s: op passed as argument
+ *
+ * Return: pointer to function relating to the op given as parameter
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,48 @@
+/*
+ * File: 3-main.c
+ * Auth: John Enweani
+ */
+
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	op = argv[2];
+
+	if ((*op == '/' && n2 == 0) ||
+	    (*op == '%' && n2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", get_op_func(op)(n1, n2));
+
+	return (0);
+}
