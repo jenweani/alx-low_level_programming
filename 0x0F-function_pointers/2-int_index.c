b0ad93e4f1597d7d269238fd6ba58b2401738707
jenweani <jenweani@gmail.com>
2023-04-19 19:38:08 -0700

    added int index task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+/*
+ * File: 2-int_index.c
+ * Auth: John Enweani
+ */
+
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer
+ * @array: array to be searched
+ * @size: number of elements in array
+ * @cmp: pointer to the function to be used to compare values
+ *
+ * Return: returns the index of the first element for which
+ *  the cmp function does not return 0, -1 if no elements matches
+ *   or if size <= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
